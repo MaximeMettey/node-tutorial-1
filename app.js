1ab927912abbd42851274ba9261b16044c015457
Maxime Mettey <maxime.mettey@gmail.com>
2023-04-29 15:51:39 +0200

    use OS module

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const path = require('node:path');
+const os = require('node:os');
 const logger = require('./logger');
 
+// First function
 function sayHello(name) {
     console.log('Hello ' + name + '!');
 }
 
 sayHello('Maxime');
+
+// Use logger module
 logger('Welcome');
 
+// Use path module
 var pathObj = path.parse(__filename);
-console.log(pathObj);
+console.log(pathObj.dir);
+
+// Use OS module
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
