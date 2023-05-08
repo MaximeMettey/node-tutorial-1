7ec4b895417b780066f6e7239fff5d4b29e090e1
Maxime Mettey <maxime.mettey@gmail.com>
2023-05-08 12:44:36 +0200

    publish npm package and use it in a small app

---
+++ lion-lib/index.js
@@ -0,0 +1,4 @@
+module.exports.add = (a, b) => { return a + b };
+module.exports.multiply = (a, b) => { return a * b };
+module.exports.sub = (a, b) => { return a - b };
+module.exports.divide = (a, b) => { return a / b };
---
+++ node-app/index.js
@@ -0,0 +1,10 @@
+var fp = require('maxime-mettey-first-package');
+var result = fp.add(1, 4);
+var result2 = fp.multiply(2, 5);
+var result3 = fp.divide(2, 5);
+var result4 = fp.sub(2, 5);
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
